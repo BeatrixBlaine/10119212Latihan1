f46d6a5cdcda93120e7d52ad4b04cff354f36b51
Irsyad Ibrahim <muhammadirsyadibrahim21@gmail.com>
2022-04-19 17:37:11 +0700

    Latihan 1

---
+++ app/src/main/java/com/example/a10119212latihan1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a10119212latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+// Nama : Muhammad Irsyad Ibrahim
+// NIM  : 10119212
+// Kelas: IF-6
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
